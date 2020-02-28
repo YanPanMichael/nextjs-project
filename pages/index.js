acf2e0de33dd9c5223fed4d167b65dfcb9fa1ac7
YanPanMichael <yanpancsu@icloud.com>
2020-02-28 23:05:36 +0800

    link as

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,20 @@ import "../css/index.css";
 
 export default () => {
   function handleRouter() {
-    Router.push({
-      pathname: '/subs/b',
-      query: {
-        id: 2
-      }
-    }, '/subs/b/2');
+    Router.push(
+      {
+        pathname: "/subs/b",
+        query: {
+          id: 2
+        }
+      },
+      "/subs/b/2"
+    ); //刷新会出现问题，因为这种方式只是浏览器显示映射而已，浏览器内部并没有这个路径，解决方法是Koa router加入映射配置
   }
 
   return (
     <>
+      {/* as的加入只是更改地址显示 刷新会出现问题，因为这种方式只是浏览器显示映射而已，浏览器内部并没有这个路径，解决方法是Koa router加入映射配置 */}
       <Link href="/a?id=1" as="/a/1" title="jump to A">
         <Button>Jump to A page</Button>
       </Link>
