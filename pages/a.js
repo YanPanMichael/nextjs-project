e040c04faf4960908fef94db681e52623d6943bc
YanPanMichael <yanpancsu@icloud.com>
2020-03-01 20:39:02 +0800

    getInitialProps async

--- pages/a.js
+++ pages/a.js
@@ -20,6 +20,22 @@ events.forEach(event => {
   Router.events.on(event, makeEvent(event))
 })
 
-const A = ({ router }) => <Comp>A {router.query.id}</Comp>;
+const A = ({ router, name }) => <Comp>A {router.query.id} {name}</Comp>;
+
+A.getInitialProps = async () => {
+
+  // return {
+  //   name: 'Rojey'
+  // }
+  console.log('...............')
+  const p = new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        name: 'JIM'
+      })
+    }, 1000);
+  })
+  return await p
+}
 
 export default withRouter(A);
